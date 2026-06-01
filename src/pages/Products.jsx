c0c8fc45e0d1d29463cb8b31f511882b8dc0bf3b
Gustav Lenander <gustav.lenander@icloud.com>
2026-06-01 18:13:41 +0200

    Server Test

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from '../components/NavBar'
 import Footer from '../components/Footer'
 import ProductCard from '../components/ProductCard'
 import ProductFilter from '../components/FilterBar'
 import './Products.css'
-import img1 from '../images/img1.png'
-import img2 from '../images/img2.png'
-
-const produkter = [
-  { id: 1, name: 'Big Sticker', price: 99, category: 'Big Stickers', image: img2 },
-  { id: 2, name: 'Big Sticker 2', price: 99, category: 'Big Stickers', image: img2 },
-  { id: 3, name: 'smal sticker 1', price: 199, category: 'Smal Stickers', image: img1 },
-  { id: 4, name: 'Smal sticker 2', price: 199, category: 'Smal Stickers', image: img1 },
-  { id: 5, name: 'Smal Sticker 3', price: 299, category: 'Smal Stickers', image: img2 },
-  { id: 6, name: 'Smal Sticker 4', price: 299, category: 'Smal Stickers', image: img2 },
-]
 
 function Products() {
+  const [products, setProducts] = useState([])
   const [activeFilter, setFilter] = useState('Alla')
 
+ useEffect(() => {
+  fetch('http://localhost:3001/products')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setProducts(data)
+    })
+    .catch(err => console.error(err))
+}, [])
+
   const filteredProducts = activeFilter === 'Alla'
-    ? produkter
-    : produkter.filter(p => p.category === activeFilter)
+    ? products
+    : products.filter(p => p.category === activeFilter)
 
   return (
     <div>
