6c9a29ea34840749a313acebb277bc3183ccf999
Gustav Lenander <gustav.lenander@icloud.com>
2026-06-03 16:32:52 +0200

    kommentarer

--- Backend/middleware/validateTokenHandler.js
+++ Backend/middleware/validateTokenHandler.js
@@ -1,12 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
- //vakt
-const validateToken = asyncHandler(async (req, res, next) => {
+ //behörig request skickas vidare till controllsern, annars returneras error
+const validateToken = asyncHandler(async (req, res, next/* next ok i express klar fortsätt om token är ok*/) => {
+    // hämtar token från Authorization header
     let token;
     let authHeader = req.headers.Authorization || req.headers.authorization;
 
     if (authHeader && authHeader.startsWith("Bearer")) {
-        token = authHeader.split(" ")[1];
+        token = authHeader.split(" ")[1]; // tar ut token från "Bearer <token>"
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
             if (err) {
                 res.status(401);
